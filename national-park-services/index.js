945b8934fdee10760f43d180650e5179e1a585b1
Andrew <andrewyin1994@gmail.com>
2019-06-11 13:46:06 -0700

    altered youtube app to use nps api

--- national-park-services/index.js
+++ national-park-services/index.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global $ */
 
 // put your own value below!
 const apiKey = 'VXqGb4kdDGdgTGa7XUsX5KR3CUfogc9NUJ7O5dZj'; 
@@ -6,40 +7,44 @@ const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
-}
+  const querydata = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return querydata.join('&');
+} 
 
 function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
   $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
+  // iterate through the data array
+  for (let i = 0; i < responseJson.data.length; i++){
+    // for each video object in the data 
     //array, add a list item to the results 
     //list with the video title, description,
     //and thumbnail
     $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <a href="${responseJson.data[i].url}">Link</a>
       </li>`
-    )};
+    );
+  }
   //display the results section  
   $('#results').removeClass('hidden');
-};
+}
 
-function getYouTubeVideos(query, maxResults=10) {
+function getNationalParks(query, maxResults=10) {
   const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
+    api_key: apiKey,
+    stateCode: query,
+    limit: maxResults
+    // key: apiKey,
+    // q: query,
+    // part: 'snippet',
+    // maxResults,
+    // type: 'video'
   };
-  const queryString = formatQueryParams(params)
+  const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString;
 
   console.log(url);
@@ -62,7 +67,7 @@ function watchForm() {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    getNationalParks(searchTerm, maxResults);
   });
 }
 
